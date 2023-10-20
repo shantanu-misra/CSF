9fa41f5c119f55a5035abec8f94378e6ae0bf3cd
Shantanu Misra <smisra8@ugradx.cs.jhu.edu>
2023-10-20 09:37:20 -0400

    first draft of read and added structure

--- hw3/src/cache.cpp
+++ hw3/src/cache.cpp
@@ -1,22 +1,93 @@
 #include "cache.h"
-#include "utils.h"
-#include <iostream>
 
-Cache::Cache() : cacheBlock("") {}
+Cache::Cache(int numSets, int blocksPerSet, bool writeAllocate, bool writeBack, bool lruEviction)
+    : blocksPerSet(blocksPerSet), writeAllocate(writeAllocate), writeBack(writeBack), 
+    lruEviction(lruEviction), currentCycle(0) {
+    sets.resize(numSets);
 
-void Cache::processAddress(const std::string& address) {
-    if(cacheBlock == address) {
-        std::cout << "Cache Hit for address: " << address << std::endl;
-    } else {
-        if(cacheBlock == "") {
-            std::cout << "Cache Miss and Load for address: " << address << std::endl;
-        } else {
-            std::cout << "Cache Miss and Replacement for address: " << address << std::endl;
+    for (int i = 0; i < numSets; ++i) {
+        sets[i].blocks.resize(blocksPerSet);
+        for (int j = 0; j < blocksPerSet; ++j) {
+            sets[i].blocks[j].valid = false;
+            sets[i].blocks[j].tag = 0;
+            sets[i].blocks[j].dirty = false;
+            sets[i].blocks[j].accessed = 0;
         }
-        cacheBlock = address;
     }
+
+    totalLoads = 0;
+    totalStores = 0;
+    loadHits = 0;
+    loadMisses = 0;
+    storeHits = 0;
+    storeMisses = 0;
+    totalCycles = 0;
 }
 
-void Cache::printDetails() {
-    std::cout << "Final Cache Block: " << cacheBlock << std::endl;
+void Cache::read(unsigned int address, int bytesInBlock) {
+    totalLoads++;  // Increment the total number of loads.
+
+    // Decode the address.
+    unsigned int index = (address / bytesInBlock) % numSets;  // Extract the set index.
+    unsigned int tag = address / (bytesInBlock * numSets);  // Extract the tag.
+
+    Set& currentSet = sets[index];  // Reference the relevant set.
+
+    bool hit = false;  // Whether the read was a cache hit.
+    unsigned int lruIndex = 0;  // To keep track of the least recently used block.
+
+    for (unsigned int i = 0; i < blocksPerSet; ++i) {
+        if (currentSet.blocks[i].valid && currentSet.blocks[i].tag == tag) {
+            // Cache hit!
+            hit = true;
+            currentSet.blocks[i].accessedCounter = currentCycle;  // Update the accessed counter for LRU.
+            loadHits++;  // Increment the number of load hits.
+            break;
+        }
+
+        // Check for the least recently used block.
+        if (currentSet.blocks[i].accessedCounter < currentSet.blocks[lruIndex].accessedCounter) {
+            lruIndex = i;
+        }
+    }
+
+    if (!hit) {
+        // Cache miss.
+        loadMisses++;  // Increment the number of load misses.
+        totalCycles += MEMORY_ACCESS_CYCLES;  // Simulate the time taken to fetch from main memory.
+
+        // Evict the least recently used block (LRU).
+        Block& evictedBlock = currentSet.blocks[lruIndex];
+
+        if (evictedBlock.valid && evictedBlock.isDirty) {
+            totalCycles += MEMORY_ACCESS_CYCLES;  // Simulate the time taken to write back to memory.
+            evictedBlock.isDirty = false;  // Reset the dirty flag.
+        }
+
+        // Load the data into the cache.
+        evictedBlock.valid = true;
+        evictedBlock.tag = tag;
+        evictedBlock.accessedCounter = currentCycle;  // Update the accessed counter.
+    }
+
+    totalCycles++;  // Simulate the time taken to access the cache.
+
+    // You may want to update the current cycle if you're tracking it.
+    currentCycle++;
+}
+
+
+void Cache::write(unsigned int address, int bytesInBlock) {
+    // TODO: Implement the write logic based on the cache settings.
+    // handle LRU eviction and the accessed counters.
+}
+
+void Cache::printSummary() {
+    std::cout << "Total loads: " << totalLoads << std::endl;
+    std::cout << "Total stores: " << totalStores << std::endl;
+    std::cout << "Load hits: " << loadHits << std::endl;
+    std::cout << "Load misses: " << loadMisses << std::endl;
+    std::cout << "Store hits: " << storeHits << std::endl;
+    std::cout << "Store misses: " << storeMisses << std::endl;
+    std::cout << "Total cycles: " << totalCycles << std::endl;
 }
--- hw3/src/cache.h
+++ hw3/src/cache.h
@@ -1,16 +1,45 @@
 #ifndef CACHE_H
 #define CACHE_H
 
-#include <string>
+#include <vector>
 
-class Cache {
-public:
-    Cache();
-    void processAddress(const std::string& address);
-    void printDetails();
-private:
-    // Assuming a simple direct-mapped cache for illustration
-    std::string cacheBlock;  // Stores a single address for simplicity
+const int MEMORY_ACCESS_CYCLES = 100;
+
+struct Block {
+    bool valid;
+    unsigned int tag;
+    bool dirty;  // For write-back policy
+    unsigned long long accessedCounter;  // LRU counter
 };
 
-#endif
+struct Set {
+    std::vector<Block> blocks;
+};
+
+class Cache {
+private:
+    std::vector<Set> sets;
+    int numSets;
+    int blocksPerSet;
+    bool writeAllocate;
+    bool writeBack;
+    bool lruEviction;
+
+    unsigned long currentCycle;
+
+    unsigned long totalLoads;
+    unsigned long totalStores;
+    unsigned long loadHits;
+    unsigned long loadMisses;
+    unsigned long storeHits;
+    unsigned long storeMisses;
+    unsigned long totalCycles;
+
+public:
+    Cache(int numSets, int blocksPerSet, bool writeAllocate, bool writeBack, bool lruEviction);
+    void read(unsigned int address, int bytesInBlock);
+    void write(unsigned int address, int bytesInBlock);
+    void printSummary();
+};
+
+#endif // CACHE_H
