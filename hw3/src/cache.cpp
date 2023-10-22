d8775d83fcbbe62489580d2c5108a71abcb1d84a
Shantanu Misra <smisra8@ugradx.cs.jhu.edu>
2023-10-22 15:03:52 -0400

    added comments, partial write method, updated main

--- hw3/src/cache.cpp
+++ hw3/src/cache.cpp
@@ -1,20 +1,24 @@
 #include "cache.h"
+#include <iostream>
 
+// Constructor to initialize cache parameters and counters.
 Cache::Cache(int numSets, int blocksPerSet, bool writeAllocate, bool writeBack, bool lruEviction)
-    : blocksPerSet(blocksPerSet), writeAllocate(writeAllocate), writeBack(writeBack), 
-    lruEviction(lruEviction), currentCycle(0) {
+    : numSets(numSets), blocksPerSet(blocksPerSet), writeAllocate(writeAllocate), 
+      writeBack(writeBack), lruEviction(lruEviction), currentCycle(0) {
     sets.resize(numSets);
 
+    // Initializing each block in each set.
     for (int i = 0; i < numSets; ++i) {
         sets[i].blocks.resize(blocksPerSet);
         for (int j = 0; j < blocksPerSet; ++j) {
-            sets[i].blocks[j].valid = false;
+            sets[i].blocks[j].valid = false;  // Initially, block contains no valid data.
             sets[i].blocks[j].tag = 0;
-            sets[i].blocks[j].dirty = false;
-            sets[i].blocks[j].accessed = 0;
+            sets[i].blocks[j].dirty = false;  // No pending writes.
+            sets[i].blocks[j].accessedCounter = 0;  // For LRU.
         }
     }
 
+    // Resetting all counters to 0.
     totalLoads = 0;
     totalStores = 0;
     loadHits = 0;
@@ -24,64 +28,108 @@ Cache::Cache(int numSets, int blocksPerSet, bool writeAllocate, bool writeBack, 
     totalCycles = 0;
 }
 
+// Function to handle a memory read request.
 void Cache::read(unsigned int address, int bytesInBlock) {
-    totalLoads++;  // Increment the total number of loads.
+    totalLoads++;
 
-    // Decode the address.
-    unsigned int index = (address / bytesInBlock) % numSets;  // Extract the set index.
-    unsigned int tag = address / (bytesInBlock * numSets);  // Extract the tag.
+    // Extracting set index and tag from the address.
+    unsigned int index = (address / bytesInBlock) % numSets;
+    unsigned int tag = address / (bytesInBlock * numSets);
 
-    Set& currentSet = sets[index];  // Reference the relevant set.
+    Set& currentSet = sets[index];
 
-    bool hit = false;  // Whether the read was a cache hit.
-    unsigned int lruIndex = 0;  // To keep track of the least recently used block.
+    bool hit = false;
+    unsigned int lruIndex = 0;
 
+    // Check each block in the set to find a hit or identify the LRU block.
     for (unsigned int i = 0; i < blocksPerSet; ++i) {
         if (currentSet.blocks[i].valid && currentSet.blocks[i].tag == tag) {
-            // Cache hit!
             hit = true;
-            currentSet.blocks[i].accessedCounter = currentCycle;  // Update the accessed counter for LRU.
-            loadHits++;  // Increment the number of load hits.
+            if (lruEviction) {
+                currentSet.blocks[i].accessedCounter = currentCycle;  // LRU policy update.
+            }
+            loadHits++;
             break;
         }
 
-        // Check for the least recently used block.
+        if (currentSet.blocks[i].accessedCounter < currentSet.blocks[lruIndex].accessedCounter) {
+            lruIndex = i;  // Track the least recently accessed block.
+        }
+    }
+
+    // If miss, we might need to evict and access main memory.
+    if (!hit) {
+        loadMisses++;
+        totalCycles += MEMORY_ACCESS_CYCLES;  // Penalty for main memory access.
+
+        Block& evictedBlock = currentSet.blocks[lruIndex];
+
+        // If block is dirty, write back to main memory.
+        if (evictedBlock.valid && evictedBlock.dirty) {
+            totalCycles += MEMORY_ACCESS_CYCLES;
+            evictedBlock.dirty = false;
+        }
+
+        // Update block with new data.
+        evictedBlock.valid = true;
+        evictedBlock.tag = tag;
+        evictedBlock.accessedCounter = currentCycle;  // Set block as recently accessed.
+    }
+
+    totalCycles++;
+    currentCycle++;  // Simulating time progression.
+}
+
+// Function to handle a memory write request.
+void Cache::write(unsigned int address, int bytesInBlock) {
+    totalStores++;
+
+    unsigned int index = (address / bytesInBlock) % numSets;
+    unsigned int tag = address / (bytesInBlock * numSets);
+
+    Set& currentSet = sets[index];
+
+    bool hit = false;
+    unsigned int lruIndex = 0;
+
+    // Check each block in the set for a hit or identify the LRU block.
+    for (unsigned int i = 0; i < blocksPerSet; ++i) {
+        if (currentSet.blocks[i].valid && currentSet.blocks[i].tag == tag) {
+            hit = true;
+            if (lruEviction) {
+                currentSet.blocks[i].accessedCounter = currentCycle;  // LRU policy update.
+            }
+            storeHits++;
+            break;
+        }
+
         if (currentSet.blocks[i].accessedCounter < currentSet.blocks[lruIndex].accessedCounter) {
             lruIndex = i;
         }
     }
 
+    // If miss, similar steps as in read but more nuances based on write policies.
     if (!hit) {
-        // Cache miss.
-        loadMisses++;  // Increment the number of load misses.
-        totalCycles += MEMORY_ACCESS_CYCLES;  // Simulate the time taken to fetch from main memory.
+        storeMisses++;
+        totalCycles += MEMORY_ACCESS_CYCLES;
 
-        // Evict the least recently used block (LRU).
         Block& evictedBlock = currentSet.blocks[lruIndex];
-
-        if (evictedBlock.valid && evictedBlock.isDirty) {
-            totalCycles += MEMORY_ACCESS_CYCLES;  // Simulate the time taken to write back to memory.
-            evictedBlock.isDirty = false;  // Reset the dirty flag.
+        
+        if (evictedBlock.valid && evictedBlock.dirty) {
+            totalCycles += MEMORY_ACCESS_CYCLES;
+            evictedBlock.dirty = false;
         }
 
-        // Load the data into the cache.
         evictedBlock.valid = true;
         evictedBlock.tag = tag;
-        evictedBlock.accessedCounter = currentCycle;  // Update the accessed counter.
+        evictedBlock.accessedCounter = currentCycle;
     }
 
-    totalCycles++;  // Simulate the time taken to access the cache.
-
-    // You may want to update the current cycle if you're tracking it.
+    totalCycles++;
     currentCycle++;
 }
 
-
-void Cache::write(unsigned int address, int bytesInBlock) {
-    // TODO: Implement the write logic based on the cache settings.
-    // handle LRU eviction and the accessed counters.
-}
-
+// Function to print cache stats at the end.
 void Cache::printSummary() {
     std::cout << "Total loads: " << totalLoads << std::endl;
     std::cout << "Total stores: " << totalStores << std::endl;
--- hw3/src/main.cpp
+++ hw3/src/main.cpp
@@ -3,14 +3,43 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <cstdlib> // for std::strtol
+
+// Utility function to check if a number is a power of 2
+bool isPowerOfTwo(int n) {
+    return (n && !(n & (n - 1)));
+}
 
 int main(int argc, char* argv[]) {
-    if(argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <trace_file>" << std::endl;
+    if(argc != 9) {
+        std::cerr << "Usage: " << argv[0] << " <num_sets> <num_blocks> <bytes_in_block> <write_policy> <write_through_or_back> <eviction_policy> <trace_file>" << std::endl;
         return 1;
     }
 
-    std::string traceFilename = argv[1];
+    // Extract and process the command-line arguments
+    int numSets = std::stoi(argv[1]);
+    int blocksPerSet = std::stoi(argv[2]);
+    int bytesInBlock = std::stoi(argv[3]);
+
+    bool writeAllocate = (std::string(argv[4]) == "write-allocate");
+    bool writeBack = (std::string(argv[5]) == "write-back");
+    bool lruEviction = (std::string(argv[6]) == "lru");
+
+    // Check for configuration validity
+    if (!isPowerOfTwo(bytesInBlock) || !isPowerOfTwo(numSets)) {
+        std::cerr << "Error: Block size and number of sets must be a power of 2." << std::endl;
+        return 1;
+    }
+    if (bytesInBlock < 4) {
+        std::cerr << "Error: Block size must be at least 4." << std::endl;
+        return 1;
+    }
+    if (writeBack && !writeAllocate) {
+        std::cerr << "Error: Cannot use write-back without write-allocate." << std::endl;
+        return 1;
+    }
+
+    std::string traceFilename = argv[7];
     std::ifstream traceFile(traceFilename);
     
     if(!traceFile.is_open()) {
@@ -18,15 +47,24 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    Cache myCache;
-    std::string address;
+    Cache myCache(numSets, blocksPerSet, writeAllocate, writeBack, lruEviction);
+    std::string operation, address;
+    unsigned int addr;
     
-    while(traceFile >> address) {
-        myCache.processAddress(address);
+    while(traceFile >> operation >> address) {
+        // Convert hex string to unsigned int
+        addr = std::strtol(address.c_str(), nullptr, 16);
+
+        // Process the memory operation based on the operation type
+        if(operation == "l") {
+            myCache.read(addr, bytesInBlock);
+        } else if(operation == "s") {
+            myCache.write(addr, bytesInBlock);
+        }
     }
 
     traceFile.close();
-    myCache.printDetails();
+    myCache.printSummary();
 
     return 0;
 }
