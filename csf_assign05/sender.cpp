0f588c3cd596c546e56e4cc7591d18ed4bf45e5f
Shantanu Misra <smisra8@ugradx.cs.jhu.edu>
2023-11-22 12:53:37 -0500

    sender code minus exception handling

--- csf_assign05/sender.cpp
+++ csf_assign05/sender.cpp
@@ -21,12 +21,88 @@ int main(int argc, char **argv) {
   server_port = std::stoi(argv[2]);
   username = argv[3];
 
-  // TODO: connect to server
+  // connect to server
+  Connection conn; // Using the Connection class
+  conn.connect(server_hostname, server_port); // Connect to the server
+  if (!conn.is_open()) {
+    std::cerr << "Failed to connect to the server.\n";
+    return 1;
+  }
 
-  // TODO: send slogin message
+  // send slogin message
+  Message slogin_msg;
+  slogin_msg.tag = "slogin";
+  slogin_msg.data = username;
+  if (!conn.send(slogin_msg)) {
+    std::cerr << "Failed to send login message.\n";
+    return 1;
+  }
 
-  // TODO: loop reading commands from user, sending messages to
-  //       server as appropriate
+  // Wait for server response after slogin
+  Message response;
+  if (!conn.receive(response) || conn.get_last_result() == Connection::EOF_OR_ERROR) {
+    std::cerr << "Login failed: no response or error from server.\n";
+    return 1;
+  }
+  if (response.tag == "err") {
+    std::cerr << "Error during login: " << response.data << "\n";
+    return 1;
+  }
 
-  return 0;
+  // Main loop for reading commands and sending messages
+  std::string line;
+  while (getline(std::cin, line)) {
+    trim(line); // Function to remove leading and trailing whitespace, make sure it is defined
+    if (line.empty()) continue;
+
+    Message msg;
+    if (line[0] == '/') { // Command
+      std::string cmd = line.substr(1);
+      size_t spaceIndex = cmd.find(' ');
+      msg.tag = spaceIndex != std::string::npos ? cmd.substr(0, spaceIndex) : cmd; // Extract the command
+      msg.data = spaceIndex != std::string::npos ? cmd.substr(spaceIndex + 1) : ""; // Extract the data if present
+
+      if (msg.tag == "quit") {
+        if (!conn.send(msg)) {
+          std::cerr << "Failed to send quit command.\n";
+          break;
+        }
+        // Wait for quit ack before closing
+        if (!conn.receive(msg) || msg.tag != "ok") {
+          std::cerr << "Quit not acknowledged by server.\n";
+          break;
+        }
+        break;
+      }
+    } else { // Regular message
+      msg.tag = "sendall";
+      msg.data = line;
+    }
+
+    // Send the message to the server
+    if (!conn.send(msg)) {
+      std::cerr << "Failed to send message: " << line << "\n";
+      break; // Exit the loop if send fails
+    }
+
+    // Wait for server response
+    if (!conn.receive(msg) || conn.get_last_result() == Connection::EOF_OR_ERROR) {
+      std::cerr << "Server error or connection closed.\n";
+      break;
+    }
+
+    // Check if the server response is an error
+    if (msg.tag == "err") {
+      std::cerr << "Error: " << msg.data << "\n";
+      continue; // Continue to process new commands
+    } else if (msg.tag != "ok") {
+      std::cerr << "Unexpected response from server: " << msg.data << "\n";
+      continue; // Continue to process new commands
+    }
+
+    // If an ok response is received, continue to process new commands
+  }
+
+  conn.close(); // Close the connection
+  return 0; // Successful exit
 }
