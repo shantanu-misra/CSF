a5d8fb8753ecabf687018e8329785d5f2571a248
Shantanu Misra <smisra8@ugradx.cs.jhu.edu>
2023-11-25 08:08:42 -0500

    rec

--- csf_assign05/receiver.cpp
+++ csf_assign05/receiver.cpp
@@ -19,10 +19,15 @@ int main(int argc, char **argv) {
   std::string room = argv[4];
 
   Connection conn;
-  int server_port_num = std::stoi(server_port);
-  conn.connect(server_hostname, server_port_num);
-  if (!conn.is_open()) {
-    std::cerr << "Failed to connect to the server.\n";
+  try {
+    int server_port_num = std::stoi(server_port);
+    conn.connect(server_hostname, server_port_num);
+    if (!conn.is_open()) {
+      std::cerr << "Failed to connect to the server.\n";
+      return 1;
+    }
+  } catch (const std::exception& e) {
+    std::cerr << "Error: " << e.what() << "\n";
     return 1;
   }
 
@@ -35,8 +40,13 @@ int main(int argc, char **argv) {
 
   // Wait for server response after rlogin
   Message response;
-  if (!conn.receive(response) || response.tag == "err") {
-    std::cerr << "Error during rlogin: " << response.data << "\n";
+  if (!conn.receive(response)) {
+    std::cerr << "Failed to receive response after rlogin.\n";
+    return 1;
+  }
+
+  if (response.tag == "err") {
+    std::cerr << response.data << "\n";
     return 1;
   }
 
@@ -48,36 +58,39 @@ int main(int argc, char **argv) {
   }
 
   // Wait for server response after join
-  if (!conn.receive(response) || response.tag == "err") {
-    std::cerr << "Error during join: " << response.data << "\n";
+  if (!conn.receive(response)) {
+    std::cerr << "Failed to receive response after join.\n";
+    return 1;
+  }
+
+  if (response.tag == "err") {
+    std::cerr << response.data << "\n";
     return 1;
   }
 
   // Main loop for reading messages from server
-while (true) {
-  if (!conn.receive(response)) {
-    std::cerr << "Connection closed by server or error occurred.\n";
-    return 1; // Exiting with non-zero status due to error
-  }
+  while (true) {
+    if (!conn.receive(response)) {
+      std::cerr << "Connection closed by server or error occurred.\n";
+      return 1;
+    }
 
-  if (response.tag == "delivery") {
-    // Extracting the sender and message text from the response data
-    std::istringstream iss(response.data);
-    std::string room, sender, message_text;
-    getline(iss, room, ':'); // Assuming the first token is the room, which we ignore
-    getline(iss, sender, ':'); // The second token is the sender
-    getline(iss, message_text); // The rest is the message text
-    std::cout << sender << message_text << "\n";
-  } else if (response.tag == "err") {
-    std::cerr << "Error: " << response.data << "\n";
-    // If it's a join or login error, terminate the program
-    if (response.data.find("rlogin:") != std::string::npos || 
-      response.data.find("join:") != std::string::npos) {
-      return 1; // Exiting with non-zero status due to error
+    if (response.tag == "delivery") {
+      std::istringstream iss(response.data);
+      std::string sender, message_text;
+      if (!(getline(iss, sender, ':') && getline(iss, message_text))) {
+        std::cerr << "Failed to parse delivery message.\n";
+        continue;
+      }
+      std::cout << sender << ": " << message_text << std::endl;
+    } else if (response.tag == "err") {
+      std::cerr << response.data << "\n";
+      if (response.data.find("rlogin:") != std::string::npos || 
+          response.data.find("join:") != std::string::npos) {
+        return 1;
+      }
     }
   }
-}
-
 
   conn.close();
   return 0;
